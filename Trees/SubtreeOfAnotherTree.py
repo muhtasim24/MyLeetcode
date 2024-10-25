1be19e2ff9f90e6e5c55b6c7d0f70c7f7c4f3c7e
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-10-25 16:59:50 -0400

    Descriptive approach on solution

--- Trees/SubtreeOfAnotherTree.py
+++ Trees/SubtreeOfAnotherTree.py
@@ -1,18 +1,31 @@
 '''
-given 2 binary trees, root and subRoot
-We have to determine if subRoot is a subTree of root
-this question kind of builds off SameTree problem
+so we are given the roots of 2 binary trees
+we have to figure out if the subRoot is a subTree of root
+So subRoot would be a subTree of root IF:
+ - subRoot exists within root
 
-We can recursively solve this with DFS,
-we can compare subtrees of root to subRoot(T)
+how can we do this?
+so if it is a subTree that means the same tree exists within the root tree
+so we could create a helper function to compare a subtree of root and subRoot
+and if its not a subRoot, we can recursivly call our main function on the left / right subtree of root
 
-we can build a helper fucntion to compare the subtrees
-if the subtrees are not the same, we can just check the left or right subtrees of the root tree (S)
+so we could implement helper function sameTree:
+if both are NULL, then they are the same
+if they are non NULL, and their values are the same
+ - we recursvily call sameTree on both trees left and right children
+ - return the result of the recrsvie calls, both left and right sides should be true to be the same
 
-Edge Cases:
-if T (subroot) is NULL, we can return True because a NULL Tree is still a subtree of a tree
-however if root is NULL, T can never be a subtree of NULL
+now for our main function
+we have to worry about some cases
+if subRoot is an empty tree, we can automatically reutnr Ture
+because an empty tree is always a subTree of any given tree
 
+However, if our root tree is empty, and our subRoot tree is not empty, that cant happen
+the subRoot wouldnt exist in an empty tree
+
+now if their values are the same, we can call sameTree on the root and SubRoot
+
+if they arent, we can rescurivly call our main function on left/right child of root, and pass subRoot
 
 '''
 
@@ -26,33 +39,35 @@ however if root is NULL, T can never be a subtree of NULL
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        #if subRoot is empty, that can be a subTree of root, so its true
+        # if subRoot is empty, we can automatically retunr True
         if not subRoot:
             return True
-        #however, if root is empty, subRoot can never be a subtree of the root
+        # now if above condition fails, as in subRoot is not Empty, but root is, subRoot cant be a subTree of root
         if not root:
             return False
         
-        #if they root and subRoot are the same, return True, we found the subtree
-        if self.sameTree(root, subRoot):
+        # call sameTree on both roots, to check if they are the sameTree
+        # no point of checking if they have same vals to call sameTree, just see if sameTree is true or false
+        if self.isSametree(root, subRoot):
             return True
-        
-        #if we didnt find the subtree, we can check the Left subtree and Right SubTree of the root
+
+        # if they arent the same val, we can recursively call isSubTree on root's left / right child
+        # and only one subTree has to be True, for us to find it 
+        # so either left subtree of root has subRoot OR right subTree of root has subRoot as a subTree
+        # if not, subRoot does not exist, so instead of AND to compare if its same Tree
+        # we do OR to check if the subTree exists within root tree
         return (self.isSubtree(root.left, subRoot) or
-            self.isSubtree(root.right, subRoot))
-        
-    
-    def sameTree(self, s, t):
-        # if both nodes are NULL, return True
+                self.isSubtree(root.right, subRoot))
+
+
+    def isSametree(self, s, t):
+        #if both are NULL, they are the same
         if not s and not t:
             return True
-        
-        #if both are not empty and they have the same value
-        # check their children
-        if s and t and s.val == t.val:
-            return (self.sameTree(s.left, t.left) and
-                self.sameTree(s.right, t.right))
 
-        # if the above statements never execute then the tree is not the same        
+        # if both are not empty, and their vals are the same, run sameTree on both children
+        if s and t and s.val == t.val:
+            return (self.isSametree(s.left, t.left) and
+                    self.isSametree(s.right, t.right))
+
         return False
-        
