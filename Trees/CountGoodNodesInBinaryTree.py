c928e9e87f7d82f43b82ecb866d4c03c520493b1
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-10-29 17:54:19 -0400

    Showing initial failed approach, and coming up with correct solution from trial and error

--- Trees/CountGoodNodesInBinaryTree.py
+++ Trees/CountGoodNodesInBinaryTree.py
@@ -1,44 +1,81 @@
+'''
+we are given the root of a binary tree
+we have to find all the good nodes in the tree
+a good node is a node:
+- from the path from the root to the Node, there are no nodes with a value greater than X
+
+can we do this with BFS, 
+
+im assumign the root node is always a good node
+so how can we do this
+
+                3
+         1.          4
+     3           1      5
+
+so we start at 3, thats already a good node
+initlilize queue with 3
+right now the max is 3, we keep track of the max = 3
+then add its neighbors, 1 and 4
+when we look at 1, compare to max, 
+if node > max:
+    good_node += 1
+else just keep going
+now that was popped
+we look at the right child 4, max is still 3
+if node(4) > max(3):
+    good_node += 1 TRUE
+now we add their children
+new max for right side is 4, but for left side its still 3
+so maybe when we put nodes into our queue, we also add in the max
+
+so [node, maxVal]
+if node >= maxVal: so node is larger so we can update maxVal to node
+    good_nodes += 1
+    maxVal = node
+#add neighbors
+node.left, maxVal
+node.right, maxVal
+
+so you cant put in multiple values in with a queue
+so we can actually do a recursive preOrder Traversal DFS
+
+we can create a dfs helper fucntoin, pass in the node, and current maxVal that we were talking about
+and res = 1, if node.val > maxVal
+and update the maxVal
+if not res = 0
+
+after we handle that, we recursively call dfs on the left and right subtrees
+and return res + left + right
+this should return and result of all the good nodes in both subtrees
+
+
+'''
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
-'''
-we are given a binary tree, we have to return the # of nodes that are good nodes
-good nodes are for example:
-    on the path from root to the node itself, there are no nodes before the current node that are larger
-    than the node we are currently at
-    As in, the node we are currently at has to be equal to the largest, or the largest node 
-    we've seen on the path so far
-
-        3
-    1      4
-  3      1   5
-
-the root node is always a good node, since its the start of the path
-4 is a good node because on the path, there are no nodes larger than 4
-1 is NOT a good node on both sides, because 3 is larger than 1, and 4 is larger than 1
-
-so we can do a recursive DFS, preorder traversal
-create a helper function that will let us pass the node, and max Node val we've encountered so far
-if currnet node >= to maxVal, then res =1 else 0
-run dfs on left children and then run dfs on right children
-'''
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        # if empty, no nodes, so 0
         
         def dfs(node, maxVal):
             if not node:
                 return 0
             
-            res = 1 if node.val >= maxVal else 0
-            maxVal = max(node.val, maxVal) #keep track of largest node we've seen on the path
-            res += dfs(node.left, maxVal) #dfs on the left, add # of good nodes returned to res
-            res += dfs(node.right, maxVal) #dfs on the right, add to # of good nodes returned to res
-
-            return res
+            if node.val >= maxVal:
+                res = 1
+                maxVal = node.val
+            else:
+                res = 0
+            
+            left = dfs(node.left, maxVal)
+            right = dfs(node.right, maxVal)
         
+            return res + left + right
+    
         return dfs(root, root.val)
